7ddbe5c76f287e654a0cbd3f88830ae8b58603b8
Leeban <leeban.xasan@outlook.com>
2025-08-19 23:32:05 +0100

    updates terraform configs

--- terraform/environment/variables.tf
+++ terraform/environment/variables.tf
@@ -1,3 +1,36 @@
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string 
+}
+
+variable "azs" {
+  type = list(string)
+}
+
+variable "public_subnet_cidr" {
+  type = list(string)
+}
+
+variable "private_subnet_cidr" {
+  type = list(string)
+}
+
+variable "alb_security_name" {
+  type = string 
+}
+
+variable "target_group_name" {
+  type = string
+}
+
 variable "container_image" {
+  type = string
+}
+
+variable "ecs_service_name" {
   type = string
 }
