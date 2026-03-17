cdd8dff272c91914e99a767fc570d9030c4a281c
Leeban <leeban.xasan@outlook.com>
2026-03-17 05:37:26 +0000

    minor: remove SAN acm module to resolve error

--- terraform/modules/acm/main.tf
+++ terraform/modules/acm/main.tf
@@ -2,8 +2,6 @@ resource "aws_acm_certificate" "main" {
   domain_name       = var.record_name
   validation_method = var.validation_method
 
-  subject_alternative_names = ["*.${var.zone_name}"]
-
   lifecycle {
     create_before_destroy = true
   }
@@ -20,14 +18,13 @@ data "aws_route53_zone" "main" {
 
 resource "aws_route53_record" "cert_val" {
   for_each = {
-    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.resource_record_name => {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
   }
 
-  allow_overwrite = true
   zone_id         = data.aws_route53_zone.main.zone_id
   name            = each.value.name
   type            = each.value.type
